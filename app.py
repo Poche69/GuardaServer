a6c11f2c308ef3a7a39886ed2549bc5ca36b97bf
Poche69 <pocchiachecca69@gmail.com>
2025-04-09 17:50:26 +0200

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, Response
 from playlist_generator import playlist_cache, start_background_updater
 
 app = Flask(__name__)
-start_background_updater()
+start_background_updater()  # Avvia subito la generazione e l’updater
 
 @app.route('/')
 def index():
