4a3f38daf45403bfb33e847968027b9c90c2ca88
Poche69 <pocchiachecca69@gmail.com>
2025-04-09 17:33:17 +0200

    Update playlist_generator.py

--- playlist_generator.py
+++ playlist_generator.py
@@ -90,4 +90,5 @@ def serve_playlist():
 
 if __name__ == '__main__':
     start_background_updater()
-    serve(app, host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))  # Usa la porta impostata da Render
+    serve(app, host='0.0.0.0', port=port)
